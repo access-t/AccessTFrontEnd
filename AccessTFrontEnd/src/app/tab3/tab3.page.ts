642dd7d3fd2d053b27300898a29a40203cf4d97b
Claire Hough <claire.n.hough@gmail.com>
2020-10-06 23:01:15 -0500

    worked on New Collection Page UI, 'controls' property for errors not working

--- AccessTFrontEnd/src/app/tab3/tab3.page.ts
+++ AccessTFrontEnd/src/app/tab3/tab3.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnyARecord } from 'dns';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page{
 
-  constructor() {}
+  newCollectionForm:FormGroup;
+
+  constructor( private fb: FormBuilder) {
+    this.newCollectionForm = this.fb.group({
+      phrase: ['', Validators.compose([Validators.required, Validators.maxLength(30), Validators.pattern("^[a-zA-Z0-9 -'?]*$")])]
+      // image: []
+    });
+  }
+
+  saveCollection(){
+
+  }
 
 }
