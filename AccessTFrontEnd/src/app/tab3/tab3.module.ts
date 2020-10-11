26c78f0534cc11387edd807b084722fd1ea833df
Claire Hough <claire.n.hough@gmail.com>
2020-10-10 22:43:31 -0500

    fixed form error displays

--- AccessTFrontEnd/src/app/tab3/tab3.module.ts
+++ AccessTFrontEnd/src/app/tab3/tab3.module.ts
@@ -1,11 +1,12 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 
 import { Tab3PageRoutingModule } from './tab3-routing.module'
 
--- AccessTFrontEnd/src/app/tab3/tab3.page.ts
+++ AccessTFrontEnd/src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AnyARecord } from 'dns';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -9,10 +9,10 @@ import { AnyARecord } from 'dns';
 })
 export class Tab3Page{
 
-  newCollectionForm:FormGroup;
+  public newCollectionForm:FormGroup;
 
   constructor( private fb: FormBuilder) {
-    this.newCollectionForm = this.fb.group({
+    this.newCollectionForm = fb.group({
       phrase: ['', Validators.compose([Validators.required, Validators.maxLength(30), Validators.pattern("^[a-zA-Z0-9 -'?]*$")])]
       // image: []
     });
