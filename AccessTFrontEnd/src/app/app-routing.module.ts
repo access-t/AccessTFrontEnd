f5d4f5a5ba54ae57c1984ef6fd9960aff58c428d
Omer Elnour <ome43b@mail.missouri.edu>
2020-11-14 15:58:11 -0600

    login and register page

--- AccessTFrontEnd/src/app/app-routing.module.ts
+++ AccessTFrontEnd/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AddCollectionComponent as AddCollectionComponent } from './pages/addcollection/addcollection.component';
-import { Tab3PageModule } from './tab3/tab3.module';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: "addcollection", component: AddCollectionComponent }
+  { path: "addcollection", component: AddCollectionComponent },
+  { path: "login", component: LoginComponent }
 ];
 @NgModule({
   imports: [
