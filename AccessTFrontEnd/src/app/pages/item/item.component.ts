47f39158071aabd0ca70bbf1e8001687bf215d8e
Omer Elnour <ome43b@mail.missouri.edu>
2020-11-15 12:26:07 -0600

    Text to Speech

--- AccessTFrontEnd/src/app/pages/item/item.component.ts
+++ AccessTFrontEnd/src/app/pages/item/item.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { PageService } from 'src/app/page.service';
 import { Item } from 'src/app/types';
+import Speech from "speak-tts"
 
 @Component({
   selector: 'app-item',
@@ -11,11 +11,15 @@ import { Item } from 'src/app/types';
 })
 export class ItemComponent implements OnInit {
   private items: Array<Item> = [];
+  private speech: Speech;
 
   constructor(private navCtrl: NavController, private page: PageService) { }
 
   ngOnInit() {
     this.items = this.page.pageData["items"];
+    this.speech = new Speech();
+    if (this.speech.hasBrowserSupport())
+      this.speech.init({ "voice": "Google US English", "rate": 0.75 }).then(data => console.log(data));
   }
 
   goHome() {
@@ -25,5 +29,9 @@ export class ItemComponent implements OnInit {
   addItem() {
     this.page.pageData = { "collection_name": this.page.pageData.name, "items": this.items };
     this.navCtrl.navigateForward("/additem");
+  }
+
+  speak(item) {
+    this.speech.speak({ text: item.name });
   }
 }
